18078100ffdc20bbe962ea2e0ff4819a84c9a630
Alan Mendelevich <ailon@ailon.org>
2010-01-06 15:28:01 +0200

    added universal (WPF/Silverlight) helper class to find elements in VisualTree

---
+++ QuickChartsWPF/TreeHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace AmCharts.Windows.QuickCharts
+{
+    /// <summary>
+    /// Helper class to unify certain aspects of navigating VisualTree in WPF and Silvelright.
+    /// </summary>
+    public static class TreeHelper
+    {
+        /// <summary>
+        /// Finds object in control's template by it's name.
+        /// </summary>
+        /// <param name="name">Objects name.</param>
+        /// <param name="templatedParent">Templated parent.</param>
+        /// <returns>Object reference if found, null otherwise.</returns>
+        public static object TemplateFindName(string name, FrameworkElement templatedParent)
+        {
+#if SILVERLIGHT
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(templatedParent); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(templatedParent, i);
+                if (child is FrameworkElement)
+                {
+                    if (((FrameworkElement)child).Name == name)
+                    {
+                        return child;
+                    }
+                    else
+                    {
+                        object subChild = TreeHelper.TemplateFindName(name, (FrameworkElement)child);
+                        if (subChild != null && subChild is FrameworkElement && ((FrameworkElement)subChild).Name == name)
+                        {
+                            return subChild;
+                        }
+                    }
+                }
+            }
+            return null;
+#else
+            return ((Control)templatedParent).Template.FindName(name, templatedParent);
+#endif
+        }
+    }
+}
