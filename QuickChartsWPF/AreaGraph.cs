9f8b61c6e6d69689b695c8510eb07b3361b997c0
Alan Mendelevich <ailon@ailon.org>
2010-04-28 16:01:50 +0300

    FEATURE ADDED: added support for Area graphs in SerialChart

---
+++ QuickChartsWPF/AreaGraph.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Data;
+
+namespace AmCharts.Windows.QuickCharts
+{
+    /// <summary>
+    /// Facilitates rendering of area graphs.
+    /// </summary>
+    public class AreaGraph : SerialGraph
+    {
+        /// <summary>
+        /// Instantiates AreaGraph.
+        /// </summary>
+        public AreaGraph()
+        {
+            this.DefaultStyleKey = typeof(AreaGraph);
+            _areaGraph = new Polygon();
+
+            BindBrush();
+        }
+
+        private void BindBrush()
+        {
+            Binding brushBinding = new Binding("Brush");
+            brushBinding.Source = this;
+            _areaGraph.SetBinding(Polygon.FillProperty, brushBinding);
+        }
+
+        private Canvas _graphCanvas;
+        private Polygon _areaGraph;
+
+        /// <summary>
+        /// Applies control template.
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            _graphCanvas = (Canvas)TreeHelper.TemplateFindName("PART_GraphCanvas", this);
+            _graphCanvas.Children.Add(_areaGraph);
+        }
+
+        /// <summary>
+        /// Renders area graph.
+        /// </summary>
+        public override void Render()
+        {
+            _areaGraph.Points = GetAreaPoints();
+        }
+
+        private PointCollection GetAreaPoints()
+        {
+            if (Locations == null)
+                return null;
+
+            PointCollection points = new PointCollection();
+            CopyLocationsToPoints(points);
+
+            if (points.Count > 0)
+            {
+                points.Insert(0, new Point(points[0].X, GroundLevel));
+                points.Add(new Point(points[points.Count - 1].X, GroundLevel));
+            }
+            return points;
+        }
+
+        private void CopyLocationsToPoints(PointCollection points)
+        {
+            // copy Points from location cause SL doesn't support PointColleciton() with parameter
+            foreach (Point p in Locations)
+            {
+                points.Add(p);
+            }
+        }
+    }
+}
